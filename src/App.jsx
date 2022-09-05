b2556fa7c5f7866159e3c09607310292120b4014
sahitm <sahit7varma@gmail.com>
2022-09-05 17:13:24 +0530

    fix - fixed links in audio for all genres

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,126 @@ import { React, useState } from "react";
 
   const PhycTrance = [
     {
-      link: (<iframe width="100%" height="80" src="https://www.youtube.com/embed/l2RwckCRiZk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song"
+          src="https://open.spotify.com/embed/track/2MWjwvJD5Khi8XQ4YPAcTn?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "3"
     },
     {
-      link: (<iframe width="100%" height="80" src="https://www.youtube.com/embed/l2RwckCRiZk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song1"
+          src="https://open.spotify.com/embed/track/0zyel0AXT4FoaGduT7qP6b?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "4"
     },
     {
-      link: (<iframe width="100%" height="80" src="https://www.youtube.com/embed/l2RwckCRiZk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song3"
+          src="https://open.spotify.com/embed/track/4EaeZrjQTyeFRZa7WVhjkA?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "3"
     }
   ];
 
   const Indie = [
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/vsbTtf0KiNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song1"
+          src="https://open.spotify.com/embed/track/5iCY0TXNImK4hyKfcplQsg?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "5"
     },
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/vsbTtf0KiNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song2"
+          src="https://open.spotify.com/embed/track/669PEr8I3wOswY8BANBpdh?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "3"
     },
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/vsbTtf0KiNU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song3"
+          src="https://open.spotify.com/embed/track/4QK1qv4mgsfdWmMzp1Z3fx?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "4"
     }
   ];
 
   const HipHop = [
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/F1hJ5hLKiVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song1"
+          src="https://open.spotify.com/embed/track/7Ie9W94M7OjPoZVV216Xus?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "4"
     },
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/F1hJ5hLKiVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song2"
+          src="https://open.spotify.com/embed/track/7iL6o9tox1zgHpKUfh9vuC?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "4"
     },
     {
-      link: (<iframe width="560" height="315" src="https://www.youtube.com/embed/F1hJ5hLKiVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>),
+      link: (
+        <iframe
+          title="song3"
+          src="https://open.spotify.com/embed/track/503OTo2dSqe7qk76rgsbep?utm_source=generator"
+          width="100%"
+          height="80"
+          frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
+      ),
       rating: "3"
     }
   ];
